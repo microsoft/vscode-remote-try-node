6fbc68a826dc7c20f803626ab19adc5b61298d19
Haans <81927126+HanSmite@users.noreply.github.com>
2024-03-22 09:50:25 +0000

    Pending changes exported from your codespace

--- app.js
+++ app.js
@@ -1,16 +1,58 @@
+/*var http = require('http');
+var fs = require('fs');
+http.createServer(function(req,res){
+    res.writeHead(200, {'content-type':'text/html'});
+    res.writeHead(200,{'content-type':'application/json'})
+    var data = {
+        id: 444,
+        name: 'node.js'};
+    if (req.url === '/'){
+        //var html = fs.readFileSync('./plik.html','utf8');
+        //var Header = 'Home page';
+        //html = html.replace('{ Header }',Header);
+        //res.end(html);
+
+        res.writeHead(200);
+        res.end(JSON.stringify(data));
+
+    }else if(req.url === '/product'){
+        res.writeHead(200);
+        res.end('Products');
+    }else if(req.url === '/blog'){
+        res.writeHead(200);
+        res.end('Blog');
+    }else{
+        res.writeHead(404);
+        res.end('404 Not found');
+    }
+    /*
+    
+        res.writeHead(200,{'content-type':'application/json'})
+        res.end(JSON.stringify(data));
+    
+    fs.createReadStream('./plik.html').pipe(res);
+    res.writeHead(200);
+    res.end(html);
+    
+}).listen(3000);
+console.log("Server on")*/
+
+
 const express = require('express')
 const app = express()
+app.set('view engine', 'pug')
+app.use('/assets', express.static('public'))
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+  res.render('index', {query: req.query.id})
 })
-
-app.get('/contact', function (req, res) {
-    res.send('contakt')
+app.get('/contact',function(req,res){
+    res.send('contact')
 })
-
-app.get('/product', function (req, res) {
-    res.send('product')
+app.get('/product/:id',function(req,res){
+    res.send('id: ' + req.params.id)
 })
-
-app.listen(3000)
+app.get('/other',function(req,res){
+    res.send('other')
+})
+app.listen(3000);
